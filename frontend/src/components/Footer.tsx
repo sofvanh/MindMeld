b793d96c6baf73a4a3c525c49cdf070397184245
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-11-28 17:53:47 +0000

    Fix footer spacing

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer className="bg-white border-t border-slate-200 px-4 text-center h-16 flex items-center justify-center text-sm text-slate-400">
       <div className="flex justify-between w-full">
         <p className="text-slate-400">
-          Crafted by&nbsp;
-          <span className="hover:text-slate-600">Niki</span> {/* TODO Add Niki's website once it exists */}
-          &nbsp;and&nbsp;
+          Crafted by
+          <span className="hover:text-slate-600"> Niki</span> {/* TODO Add Niki's website once it exists */}
+          and&nbsp;
           <a href="https://sofiavanhanen.fi" target="_blank" rel="noopener noreferrer">Sofi</a>
         </p>
         <a href="https://github.com/sofvanh/mindmeld" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
