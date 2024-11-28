4e10e914acc26825e1e3fdf3ab9768f07d74bc22
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-11-28 16:56:57 +0000

    Fix footer link errors

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
       <div className="flex justify-between w-full">
         <p className="text-slate-400">
           Crafted by&nbsp;
-          <a target="_blank">Niki</a>
+          <span className="hover:text-slate-600">Niki</span> {/* TODO Add Niki's website once it exists */}
           &nbsp;and&nbsp;
-          <a href="https://sofiavanhanen.fi" target="_blank">Sofi</a>
+          <a href="https://sofiavanhanen.fi" target="_blank" rel="noopener noreferrer">Sofi</a>
         </p>
-        <a href="https://github.com/sofvanh/mindmeld" target="_blank" className="inline-flex items-center">
+        <a href="https://github.com/sofvanh/mindmeld" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
           <FaGithub className="w-4 h-4 mr-1" />
           Source
         </a>
