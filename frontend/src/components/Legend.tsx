56c62ba5ca8b377e910330050692e67e24f1de33
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-12-23 14:16:20 +0200

    Add legend

---
+++ frontend/src/components/Legend.tsx
@@ -0,0 +1,35 @@
+import { IMPORTANCE_COLOR, CONSENSUS_COLOR, FRAGMENTATION_COLOR } from '../utils/colors';
+
+export default function Legend() {
+  const legendItems = [
+    { color: CONSENSUS_COLOR, label: 'High consensus score', description: 'Surprising agreement' },
+    { color: FRAGMENTATION_COLOR, label: 'High fragmentation score', description: 'Surprising disagreement' },
+    { color: IMPORTANCE_COLOR, label: 'Important', description: 'Both consensus and fragmentation are high. This node seems important!' },
+  ];
+
+  // TODO Create a page explaining the colors/scores!
+  return (
+    <details className="flex flex-col px-2">
+      <summary className="py-3 sm:py-1 cursor-pointer text-sm text-stone-700 font-semibold min-h-11 sm:min-h-0 items-center justify-center">
+        Legend
+      </summary>
+      <div className="bg-white rounded-lg p-2">
+        {legendItems.map(({ color, label, description }) => (
+          <div key={label} className="flex gap-2 mt-2">
+            <div
+              className="w-4 h-4 rounded"
+              style={{
+                backgroundColor: `rgba(${color.r}, ${color.g}, ${color.b}, 0.5)`,
+              }}
+            />
+            <div className="flex flex-col">
+              <span className="text-xs text-stone-700 font-semibold">{label}</span>
+              <span className="text-xs text-stone-500 max-w-[200px]">{description}</span>
+            </div>
+          </div>
+        ))}
+        <a href="/about" className="flex items-center text-sm h-11 sm:h-auto sm:py-2">What do the scores mean?</a>
+      </div>
+    </details>
+  );
+}
