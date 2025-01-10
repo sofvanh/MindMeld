aa1c3b6e53e48e58a8e1edb0f9bbe5f0568876a8
sofvanh <23138848+sofvanh@users.noreply.github.com>
2025-01-10 14:52:39 +1300

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,10 +5,7 @@ export default function Footer() {
     <footer className="bg-white border-t border-stone-200 px-4 text-center h-16 flex items-center justify-center text-sm text-stone-400">
       <div className="flex justify-between w-full">
         <p className="text-stone-400">
-          Crafted by
-          <span className="hover:text-stone-600"> Niki</span> {/* TODO Add Niki's website once it exists */}
-          and&nbsp;
-          <a href="https://sofiavanhanen.fi" target="_blank" rel="noopener noreferrer">Sofi</a>
+          Crafted by <a href="https://mosaic-labs.org" target="_blank" rel="noopener noreferrer">Mosaic Labs</a>
         </p>
         <a href="https://github.com/sofvanh/mindmeld" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
           <FaGithub className="w-4 h-4 mr-1" />
