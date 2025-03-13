83387b3d3413c744d7b24417c9493180d66e8b27
sofvanh <23138848+sofvanh@users.noreply.github.com>
2025-03-14 10:36:33 +1300

    Rename graphs to discussions

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -4,8 +4,8 @@ import { useWebSocket } from '../contexts/WebSocketContext';
 import { buttonStyles } from '../styles/defaultStyles';
 import { useAuth } from '../contexts/AuthContext';
 import { usePageTitle } from '../hooks/usePageTitle';
-import { FeaturedGraphsList } from '../components/FeaturedGraphsList';
-import { MyGraphsList } from '../components/MyGraphsList';
+import { FeaturedDiscussionsList } from '../components/FeaturedDiscussionsList';
+import { MyDiscussionsList } from '../components/MyDiscussionsList';
 import SignInOutButton from '../components/SignInOutButton';
 
 
@@ -49,13 +49,13 @@ const HomeView: React.FC = () => {
         </p>
       </div>
       <div className="my-24">
-        <FeaturedGraphsList />
+        <FeaturedDiscussionsList />
       </div>
       {user ? (
         <>
           <div className="my-24 bg-stone-100 p-6">
-            <h2 className="mb-2">Create new graph</h2>
-            <small className="block mb-8">Graphs you create will appear under "My Graphs" below. Anyone with a link to the graph will be able to view and contribute to it.</small>
+            <h2 className="mb-2">Create new discussion</h2>
+            <small className="block mb-8">Discussions you create will appear under "My discussions" below. Anyone with a link to the discussion will be able to view and contribute to it.</small>
             {error && <p className="text-red-500 mb-4">{error}</p>}
             <form onSubmit={handleCreateGraph} className="mb-4">
               <div className="flex flex-row">
@@ -64,7 +64,7 @@ const HomeView: React.FC = () => {
                   type="text"
                   value={graphName}
                   onChange={(e) => setGraphName(e.target.value)}
-                  placeholder="Enter graph name"
+                  placeholder="Topic of discussion"
                   className="mr-2"
                 />
                 <button
@@ -77,13 +77,13 @@ const HomeView: React.FC = () => {
             </form>
           </div>
           <div className="my-24">
-            <MyGraphsList />
+            <MyDiscussionsList />
           </div>
         </>
       ) : (
         <div className="flex flex-col items-center text-center mt-14 mb-20 bg-stone-100 p-6">
           <h4>
-            Sign in to create your own graphs, contribute to existing graphs, and more!
+            Sign in to start your own discussion, contribute to existing ones, and more!
           </h4>
           <SignInOutButton />
         </div>
